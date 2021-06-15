4971ba4b7fd0e0db53bec54b7110bfcde27e32b9
nathan-tw <106306027@nccu.edu.tw>
2021-06-15 03:56:05 -0400

    rename and filter

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -2,24 +2,37 @@ import React, { useState, useEffect } from 'react';
 const Binance = require('node-binance-api');
 
 function Order() {
-    const [order, setOrder] = useState([])
+    const [trades, settrades] = useState([])
+    const [displayTrades, setDisplayTrades] = useState([])
+
+    const sixHours = 60*60*6*1000
+    const oneDay = 4*sixHours
+
+    const handleDisplaytrades = (timeDelta) => {
+        setDisplayTrades(trades.filter(trade =>new Date().getTime() - trade.time < timeDelta))
+     }
+
     useEffect(() => {
-        fetchData('ETHUSDT').then(apiOrder => {
-            setOrder(apiOrder);
+        fetchData('ETHUSDT').then(apiTrades => {
+            apiTrades = apiTrades.reverse();
+            settrades(apiTrades);
+            setDisplayTrades(apiTrades);
         })
     }, [])
     
-    console.log(order);
     return (
         <div>
-            {order.map((historyOrder, historyOrderIdx) => {
-                let time = new Date(historyOrder.time)
-                console.log(time)
-                return <div key={historyOrderIdx}>{historyOrder.price}, {historyOrder.side}, {time.toLocaleString()}</div>
+            <button onClick={() => handleDisplaytrades(sixHours) }>6hours</button>
+            <button onClick={() => handleDisplaytrades(oneDay) }>1day</button>
+            {displayTrades
+                .map((trade, tradeIdx) => {
+                    return <div key={tradeIdx}>{trade.price}, {trade.side}, {trade.qty}, {new Date(trade.time).toLocaleString()}</div>
             })}
         </div>
     )
 }
+
+
 
 const fetchData = (symbol) => {
     const apiKey = process.env.REACT_APP_BINANCE_API_KEY
@@ -30,5 +43,10 @@ const fetchData = (symbol) => {
     });
     return binance.futuresUserTrades( symbol )
 }
+
+// const computeProfit = () => {
+
+// }
+
 
 export default Order
